d69037aca229c5db8519cf4ab9a97c81ee240476
LordNinth <rr621801@gmail.com>
2024-05-12 16:04:20 +0100

    sessions

--- index.js
+++ index.js
@@ -5,16 +5,27 @@ const session = require("express-session")
 
 //this middleware sets session id and signs it with a cookie 
 app.use(session({
+    //secret is used to sign cookie
     secret:"thisSecret",
+    resave: false,
+    saveUninitialized: false,
 
 }))
 
+
 app.get("/", (req,res)=>{
-    // res.cookie("name","shin",{secure:true})
-    res.send("hello")
+    const pageViews = count++
+    res.send(`this page has been visited ${pageViews} times`)
 })
 
-
+app.get("/session",(req,res)=>{
+    if(req.session.pageViews){
+        req.session.pageViews +=1;
+    }else{
+        req.session.pageViews = 1;
+    }
+    res.send(req.session)
+})
 app.get("/cookie", (req,res)=>{
     res.send("testing secure")
 })
